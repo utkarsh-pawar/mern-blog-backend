a20e7a1831f068e60bb1deab51cceba038ff8183
Utkarsh-pawar <utkarshpawar910910@gmail.com>
2022-02-17 18:04:14 +0530

    cors error

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ import bodyParser from "body-parser";
 dotenv.config();
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.json());
-app.use(cors());
+
 
 const PORT = process.env.PORT || 5000;
 
