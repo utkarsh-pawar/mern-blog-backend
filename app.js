d7575e17cdbab167408d4a964f6e618db136fa8a
Utkarsh-pawar <utkarshpawar910910@gmail.com>
2022-02-17 18:12:05 +0530

    cors error

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import bodyParser from "body-parser";
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "https://smallpost.vercel.app" }));
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
